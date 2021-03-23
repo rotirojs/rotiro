a19137789310ce24a90c2e3ba923f2d254d48e03
Alex Everitt <alex.everitt@bytechaser.com>
2021-03-23 10:17:27 +0000

    added option to display objects with logger

--- src/services/debugger.ts
+++ src/services/debugger.ts
@@ -1,6 +1,7 @@
 import debug = require('debug');
 const rotiroDebug = debug('rotiro:debug');
 const rotiroError = debug('rotiro:error');
+const rotiroDisplay = debug('rotiro:display');
 
 export const logger = {
   debug: (message: string) => {
@@ -8,5 +9,24 @@ export const logger = {
   },
   error: (message: string) => {
     rotiroError(message);
+  },
+  display: (message: string, content: any) => {
+    // displays the content of an object
+    if (rotiroDisplay.enabled) {
+      // only process this if debugger is enabled since it could be an expensive function
+      if (typeof content === 'undefined' || content === null) {
+        return;
+      }
+      if (typeof content === 'object') {
+        try {
+          content = JSON.stringify(content);
+        } catch (ex) {
+          logger.error(`Logger Error displaying object ${ex}`);
+          return;
+        }
+      }
+
+      rotiroDisplay(`${message}: ${content}`);
+    }
   }
 };
