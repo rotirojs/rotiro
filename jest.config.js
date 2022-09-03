816f8df3cbb127715ac7ff08efcce4398dfed856
rotirojs <80029822+rotirojs@users.noreply.github.com>
2022-09-03 10:17:10 +0100

    removed map files from build output (#48)
    Co-authored-by: Alex Everitt <alex.everitt@bytechaser.com>

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1'
