a0a46b79b28cc2ff86fb75a972471731daed6099
Alex Everitt <alex.everitt@bytechaser.com>
2022-10-23 13:11:41 +0100

    Bug/api path parameters incorrect (#49)
    * added fixe to parameter order
    
    * applied formatting

--- src/utils/request-params/path-params.ts
+++ src/utils/request-params/path-params.ts
@@ -8,7 +8,9 @@ export function getPathParams(
   mappers: Mappers
 ): Record<string, ApiRequestParam> {
   const pathParams: Record<string, ApiRequestParam> = {};
+
   if (endpoint.pathParams && endpoint.pathParams.length) {
+    setParameterOrder(endpoint);
     // get the names and values from the path
     let params: string[] | null = path.match(endpoint.pattern);
     if (!params) {
@@ -30,4 +32,33 @@ export function getPathParams(
     }
   }
   return pathParams;
+}
+
+export function setParameterOrder(endpoint: ApiEndpointSchema): void {
+  if (endpoint.pathParams && endpoint.pathParams.length) {
+    const pathParts: string[] = endpoint.path.split('/');
+    const pathOrder: string[] = [];
+
+    const pathParams: Record<string, any> = {};
+    for (const pathParam of endpoint.pathParams) {
+      pathParams[pathParam.name] = pathParam;
+    }
+
+    for (const part of pathParts) {
+      if (part.startsWith(':')) {
+        let pathPart: string = part;
+        const sepPos: number = pathPart.indexOf('.');
+        if (sepPos > -1) {
+          pathPart = pathPart.substring(0, sepPos);
+        }
+        pathOrder.push(pathPart.substring(1));
+      }
+    }
+
+    const updatePathParams: any[] = [];
+    for (const pathOrderItem of pathOrder) {
+      updatePathParams.push(pathParams[pathOrderItem]);
+    }
+    endpoint.pathParams = updatePathParams;
+  }
 }
